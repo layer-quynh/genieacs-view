3a7443f5abd9c01c37d2a4337725aad59595a835
IotLab <iotlab@fit5.fit-uet.tk>
2018-11-12 17:11:54 +0700

    updateDeviceCtrl

--- app/controllers/devices.controller.js
+++ app/controllers/devices.controller.js
@@ -9,15 +9,29 @@ angular.module('genieacs')
     }
 
     getIDsDevice = function() {
-        $http.post('http://192.168.0.20:7557/devices/')
-        .then(function successCallback(data) {
-            //if(d.code == '200') {
-                console.log(data);
-            //}
-        }, function errorCallback(err) {
+        //$http.post('http://192.168.0.20:7557/devices/')
+        //.then(function successCallback(data) {
+        //    //if(d.code == '200') {
+        //        console.log(data);
+        //    //}
+        //}, function errorCallback(err) {
+        //    console.log(err);
+        ////     console.log('12344');
+        //// console.log('12345');
+        //});
+    
+	$http.jsonp('http://192.168.0.20:7557/devices/')
+
+	.success(function (data, status, headers, config) {
+
+        //$scope.details = data;
+            console.log(data);
+
+        })
+
+        .error(function (data, status, headers, config) {
+
+        //$scope.statusval = status;
             console.log(err);
-        //     console.log('12344');
-        // console.log('12345');
-        });
-    }
-})
+    })
+}})
